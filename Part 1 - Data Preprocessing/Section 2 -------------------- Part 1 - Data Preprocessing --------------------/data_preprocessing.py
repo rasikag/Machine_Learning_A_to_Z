61819405457d902ba64ef3c1ac502a1b81237ad3
rasikag <rasikagayan90@gmail.com>
2018-05-05 17:02:20 +0800

    takecare of missing values

--- Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/data_preprocessing.py
+++ Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/data_preprocessing.py
@@ -12,3 +12,11 @@ dataset = pd.read_csv('Data.csv')
 # 
 X = dataset.iloc[:, :-1].values
 # matrix of independent variables 
+y = dataset.iloc[:, 3].values
+
+# taking care of missing data 
+from sklearn.preprocessing import Imputer
+imputer = Imputer(missing_values = 'NaN', strategy= 'mean', axis = 0)
+imputer = imputer.fit(X[:,1:3])
+# replace the missing data
+X[:, 1:3] = imputer.transform(X[:, 1:3])
