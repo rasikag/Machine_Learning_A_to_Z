77d5a82fb352ec1670d1bcc9d32b18606d637c39
rasikag <rasikagayan90@gmail.com>
2018-05-07 22:56:40 +0800

    feature scalling

--- Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/data_preprocessing.R
+++ Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/data_preprocessing.R
@@ -33,4 +33,9 @@ split = sample.split(dataset$Purchased,
 training_set = subset(dataset, split == TRUE)
 test_set = subset(dataset, split == FALSE)
 
+# Feature Scaling 
+training_set[, 2:3] = scale(training_set[, 2:3])
+test_set[, 2:3] = scale(training_set[, 2:3])
+
+
 
