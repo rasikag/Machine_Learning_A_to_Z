256a3710c3753014e3e2ee57c7669f3b4efbfa5b
rasikag <rasikagayan90@gmail.com>
2018-05-08 22:19:08 +0800

    simple linear regression

---
+++ Part 2 - Regression/Section 4 - Simple Linear Regression/simple_linear_regression.py
@@ -0,0 +1,29 @@
+# Simple Linear Regression
+
+# import files 
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# import the dataset
+dataset = pd.read_csv('Salary_Data.csv')
+X = dataset.iloc[:, :-1].values
+# put the index of depending variable
+y = dataset.iloc[:, 1].values
+
+# taking care of missing data 
+from sklearn.preprocessing import Imputer
+imputer = Imputer(missing_values = 'NaN', strategy= 'mean', axis = 0)
+imputer = imputer.fit(X[:,1:3])
+
+
+
+# splitting the data set into the training set and test set 
+from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+# feature scaling 
+"""from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train) 
+X_test = sc_X.transform(X_test)"""
