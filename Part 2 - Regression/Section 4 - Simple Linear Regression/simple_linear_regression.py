d517b8fffeec9fdebb69cbe9ebcfd0e9e384057a
rasikag <rasikagayan90@gmail.com>
2018-05-08 23:15:58 +0800

    simple linear regression

--- Part 2 - Regression/Section 4 - Simple Linear Regression/simple_linear_regression.py
+++ Part 2 - Regression/Section 4 - Simple Linear Regression/simple_linear_regression.py
@@ -20,10 +20,12 @@ imputer = imputer.fit(X[:,1:3])
 
 # splitting the data set into the training set and test set 
 from sklearn.cross_validation import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 1/3, random_state = 0)
 
 # feature scaling 
 """from sklearn.preprocessing import StandardScaler
 sc_X = StandardScaler()
 X_train = sc_X.fit_transform(X_train) 
 X_test = sc_X.transform(X_test)"""
+
+from skl
