244338cdab559d8eae38e737629cdf77f382d0a0
rasikag <rasikagayan90@gmail.com>
2018-05-09 22:58:41 +0800

    simple linear regression R part 01

---
+++ Part 2 - Regression/Section 4 - Simple Linear Regression/simple_linear_regression.R
@@ -0,0 +1,23 @@
+# Simple Linear Regression 
+
+# Import the dataset 
+dataset = read.csv('Salary_Data.csv')
+
+# Create subset of data set 
+#dataset = dataset[, 2:3]
+
+# Splitting the dataset into the Training set and Test set
+
+#install.packages('caTools')
+library(caTools)
+set.seed(123)
+split = sample.split(dataset$Purchased, SplitRatio = 0.8)
+training_set = subset(dataset, split == TRUE)
+test_set = subset(dataset, split == FALSE)
+
+# Feature Scaling 
+# training_set[, 2:3] = scale(training_set[, 2:3])
+# test_set[, 2:3] = scale(training_set[, 2:3])
+
+
+
